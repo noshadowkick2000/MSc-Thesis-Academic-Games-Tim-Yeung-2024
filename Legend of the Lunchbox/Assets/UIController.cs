db8f64141d435ee2f0e4a4f4adde5cc9c2ccbbad
noshadowkick2000 <35992585+noshadowkick2000@users.noreply.github.com>
2024-10-30 14:37:48 +0100

    Refactored UI into seperate component

---
+++ Legend of the Lunchbox/Assets/UIController.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private GameObject mindUI;
+    [SerializeField] private GameObject thoughtUI;
+    [SerializeField] private GameObject controlIndicatorUI;
+    [SerializeField] private Slider timerUI;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        thoughtUI.SetActive(false);
+        mindUI.SetActive(false);
+        controlIndicatorUI.SetActive(false);
+    }
+
+    private void Idle(bool encounterOver)
+    {
+        if (encounterOver) 
+        {
+            controlIndicatorUI.SetActive(false);
+            StartCoroutine(AnimateCanvas(true)); 
+        }
+        thoughtUI?.SetActive(false);
+    }
+
+    private void StartMind()
+    {
+        StartCoroutine(AnimateCanvas(false));
+    }
+    
+    private IEnumerator AnimateCanvas(bool inverse) 
+    {
+        if (!inverse)
+            mindUI.SetActive(true);
+
+        RectTransform mt = mindUI.GetComponent<RectTransform>();
+        mt.localScale = inverse ? Vector3.one : Vector3.zero;
+
+        float duration = 0.5f; //TODO make this dependent on start time GameEngine
+
+        float startTime = Time.realtimeSinceStartup;
+        while (Time.realtimeSinceStartup < startTime + duration)
+        {
+            float x = (Time.realtimeSinceStartup - startTime) / duration;
+            if (inverse) { x = 1 - x; }
+            mt.localScale = new Vector3 (x, x, 1);
+            yield return null;
+        }
+
+        mt.localScale = inverse ? Vector3.zero : Vector3.one;
+        if (inverse)
+            mindUI.SetActive(false);
+    }
+    
+    public void StartThought() 
+    { 
+        thoughtUI.SetActive(true);
+        controlIndicatorUI.SetActive(false);
+    }
+
+    Coroutine timerRoutine;
+
+    private void EndThought(float timeOut) 
+    { 
+        thoughtUI.SetActive(false);
+        controlIndicatorUI.SetActive(true);
+
+        timerRoutine = StartCoroutine(AnimateTimer(timeOut));
+    }
+
+    private void CancelTimer()
+    {
+        StopCoroutine(timerRoutine);
+    }
+
+    private IEnumerator AnimateTimer(float timeOut)
+    {
+        float startTime = Time.realtimeSinceStartup;
+        while (Time.realtimeSinceStartup < startTime + timeOut)
+        {
+            float x = 1 - (Time.realtimeSinceStartup - startTime) / timeOut;
+            timerUI.value = x;
+            yield return null;
+        }
+    }
+    
+    private void SubscribeToEvents()
+    {
+        GameEngine.SettingUpMindStartedEvent += SettingUpMind;
+        GameEngine.ThinkingOfPropertyStartedEvent += ThinkingOfProperty;
+        GameEngine.ShowingPropertyStartedEvent += ShowingProperty;
+        GameEngine.EvaluatingInputStartedEvent += EvaluatingInput;
+    }
+
+    private void UnsubscribeFromEvents()
+    {
+        GameEngine.SettingUpMindStartedEvent -= SettingUpMind;
+        GameEngine.ThinkingOfPropertyStartedEvent -= ThinkingOfProperty;
+        GameEngine.ShowingPropertyStartedEvent -= ShowingProperty;
+        GameEngine.EvaluatingInputStartedEvent -= EvaluatingInput;
+    }
+    
+    protected virtual void SettingUpMind()
+    {
+        StartMind();
+    }
+    
+    protected virtual void ThinkingOfProperty(bool encounterOver)
+    {
+        if (encounterOver)
+            Idle(true);
+        else
+            StartThought();
+    }
+
+    protected virtual void ShowingProperty(float enemyTimeOut, Action<InputHandler.InputState> callback)
+    {
+        EndThought(enemyTimeOut);
+    }
+
+    protected virtual void EvaluatingInput()
+    {
+        CancelTimer();
+    }
+}
