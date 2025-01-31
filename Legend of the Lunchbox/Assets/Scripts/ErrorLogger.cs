35ae8bdf21c00ba6550d7827d805e420253e7bd3
noshadowkick2000 <35992585+noshadowkick2000@users.noreply.github.com>
2025-02-01 00:02:01 +0100

    Minor tweaks error logger and main menu text size

--- Legend of the Lunchbox/Assets/Scripts/ErrorLogger.cs
+++ Legend of the Lunchbox/Assets/Scripts/ErrorLogger.cs
@@ -13,16 +13,13 @@ public class ErrorLogger : MonoBehaviour
 
         // Define log file path
         logFilePath = Path.Combine(Application.streamingAssetsPath, "error_log_" + DateTime.Now.ToString("dd-MM-yyyy_HH-mm-ss") + ".txt");
-
-        // Clear previous logs (optional)
-        File.WriteAllText(logFilePath, "=== LotL ERROR LOG ===\n");
     }
 
     void HandleLog(string logString, string stackTrace, LogType type)
     {
         if (type == LogType.Error || type == LogType.Exception)
         {
-            string logEntry = $"{logString}\n{stackTrace}\n\n";
+            string logEntry = $"-----------\n{logString}\n{stackTrace}\n-----------\n";
 
             // Append to log file
             File.AppendAllText(logFilePath, logEntry);
